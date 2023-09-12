34c240f2813c51180d7e8e8798a1efba72acddca
Jeisson Lopez <jeisonma86@hotmail.com>
2023-09-11 23:02:12 -0500

    Adición de tags para mejorar puntuación de SEO

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -2,16 +2,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
-        <Html style={{height: '100%'}}>
+        <Html style={{ height: '100%' }}>
+            <title> Marvel Store </title>
             <Head>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Roboto"
-                    rel="stylesheet"
-                />
+                <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet" />
+                <meta name="description" content='Proyecto Final de Front End 3 cursado en DH que trata de una tienda online de Marvel'></meta>
             </Head>
-            <body style={{height: '100%'}}>
-            <Main />
-            <NextScript />
+            <body style={{ height: '100%' }}>
+                <Main />
+                <NextScript />
             </body>
 
         </Html>
